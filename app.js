c135d476b055002dd353d8962ac0a80d9f3626f0
jaredbucko <jaredbucko@icloud.com>
2020-04-03 20:17:12 -0700

    copied over portfolio page

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const path = require('path');
 let app = express();
 app.use(express.static(path.join(__dirname, 'build')));
-const port = process.env.PORT || '8080';
+const port = process.env.PORT || '3000';
 app.set('port', port);
 const server = http.createServer(app);
 server.listen(port, () => console.log(`Running on localhost:${port}`));
