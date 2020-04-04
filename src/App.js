ae3ab9fd37cc7f07aea48ddda819a656bb11d401
jaredbucko <jaredbucko@icloud.com>
2020-04-03 19:48:07 -0700

    revert to default app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
-import Header from './components/Header';
-import Spacer from './components/Spacer';
-import Body from './components/Body';
-import Footer from './components/Footer';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-      <Header />
-        <Spacer />
-        <Body />
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
